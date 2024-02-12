48dbc1d8e59ee5c2b5d8d3700d6022d8932f4a06
Marshal1 <marshalolimmah@gmail.com>
2024-02-12 17:25:36 -0600

    MY first commit of the Simple.java file

---
+++ simple.java
@@ -0,0 +1,7 @@
+/// your first java program because you have a very big head
+//
+//
+//
+//But why did Nigeria lose the match though, I am so pained.
+
+This should represent the actual code that makes sense.  hahahahhaahhaahaha
